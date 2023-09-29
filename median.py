abf32a944b9754c16a93894209efa2ac9c7adb76
Juntong Deng <juntong.deng@kcl.ac.uk>
2023-09-29 17:21:11 +0800

    Median algorithm

--- median.py
+++ median.py
@@ -5,8 +5,13 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        len = len(numbers)
+        if len % 2 == 0:
+            print((numbers[len // 2] + numbers[len // 2 - 1]) / 2)
+        else:
+            print(numbers[len // 2])
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
